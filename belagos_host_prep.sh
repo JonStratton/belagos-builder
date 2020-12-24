3d70f7ba8a547abc975b5f8d7917ecdc083d19ba
jgstratton <jgstratton@belagos.localdomain>
2020-12-24 07:44:10 -0600

    Fixing some issues with installer, and splitting into two stages.

--- belagos_host_prep.sh
+++ belagos_host_prep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-package_list="dnsmasq iptables-persistent qemu-system-x86 vde2 uml-utilities kpcli"
+package_list="dnsmasq iptables-persistent qemu-system-x86 vde2 uml-utilities kpcli expect build-essential libx11-dev libxt-dev"
 
 ###########
 # Install #
@@ -50,36 +50,8 @@ sudo sh -c '( iptables-save > /etc/iptables/rules.v4 )'
 sudo sh -c '( echo "net.ipv4.ip_forward = 1
 net.ipv6.ip_forward = 1" > /etc/sysctl.d/belagos-sysctl.conf )'
 
-# Create keepass db(belagos.kdbx) so we dont have to default passwords.
-expect keepass.exp
-
-# Prep the VMs
-wget http://9front.org/iso/9front-8013.d9e940a768d1.amd64.iso.gz
-gunzip 9front-8013.d9e940a768d1.amd64.iso.gz
-
-# FSSERVE
-# Creating base install image
-expect 9front_fsserve_install.exp 9front_fsserve.img 256 9front-8013.d9e940a768d1.amd64.iso 10G
-# Back it up as expect is unrelyable with curses
-cp 9front_fsserve.img 9front_fsserve.img_back
-# Set up networking and turn on PXE
-expect 9front_fsserve_configure.exp 9front_fsserve.img 256
-# Runner scripts for our VDE network after reboot
-echo "qemu-system-x86_64 -m 256 -net nic,macaddr=52:54:00:00:EE:03 -net vde,sock=/var/run/vde2/tap0.ctl -device virtio-scsi-pci,id=scsi -drive if=none,id=vd0,file=9front_fsserve.img -device scsi-hd,drive=vd0 -curses" > run_fsserve.sh
-chmod u+x run_fsserve.sh
-
-# AUTHSERVE
-# authserve and cpuserve should be PXE bootable at this point
-qemu-img create -f qcow2 9front_authserve.img 1M
-expect 9front_authserve_configure.exp 9front_authserve.img 256
-echo "qemu-system-x86_64 -m 256 -net nic,macaddr=52:54:00:00:EE:04 -net vde,sock=/var/run/vde2/tap0.ctl -device virtio-scsi-pci,id=scsi -drive if=none,id=vd0,file=9front_authserve.img -device scsi-hd,drive=vd0 -boot n -curses" > run_authserve.sh
-chmod u+x run_authserve.sh
-
-# MISC
-echo "qemu-system-x86_64 -m 256 -net nic,macaddr=52:54:00:00:EE:05 -net vde,sock=/var/run/vde2/tap0.ctl -curses" > run_terminal.sh
-chmod u+x run_cpuserve.sh
-
 # You have to use 9front's Drawterm to connect to 9front it seems
+s
 cd /opt/
 sudo wget https://code.9front.org/hg/drawterm/archive/tip.tar.gz
 sudo tar xzf tip.tar.gz
