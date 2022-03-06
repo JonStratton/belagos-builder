ea41636b7aee0f15d3b70d67550ac3fe81fd9721
jgstratton <jgstratton@cyber.lan>
2022-03-06 17:21:11 -0600

    The return of boot_wait...

--- bin/belagos_client.sh
+++ bin/belagos_client.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
+# Clean up the tail -f on exit
+trap "kill 0" 1 2 3 6
 
-script_loc=`dirname $0`
-in="$script_loc/../img/$1_in"
-out="$script_loc/../img/$1_out"
+in=""
+out=""
 
-# If no arguments, print out connections and exit.
-if [ ! $1 ]; then
-   for pipe_in in `ls -1 bin/../img/*_in`; do 
-      basename $pipe_in | sed 's/_in$//g'
-   done
-   exit 1
-fi
-
-# If argument, check pipe if exists. If not, exit
-
+# TODO, handle control-c to the tail isnt left open
 main()
 {
 tail -f $out &
@@ -27,5 +19,30 @@ EOF
 done
 }
 
+args()
+{
+script_loc=`dirname $0`
+# If argument use it if it exists
+if [ $1 ]; then
+   if [ -w "bin/../img/$1_in" ]; then
+      in="$script_loc/../img/$1_in"
+      out="$script_loc/../img/$1_out"
+   else
+      echo "Error, can not find named pipes under bin/../img/$1_*"
+   fi
+elif [ `ls -1 $script_loc/../img/*_in | wc -l` -eq 1 ]; then
+   single_in=`ls -1 $script_loc/../img/*_in`
+   base=`basename $single_in _in`
+   in="$script_loc/../img/${base}_in"
+   out="$script_loc/../img/${base}_out"
+else
+   for pipe_in in `ls -1 bin/../img/*_in`; do
+      basename $pipe_in | sed 's/_in$//g'
+   done
+   exit 1
+fi
+}
+
+args
 main
 exit 0
--- bin/boot_wait.sh
+++ bin/boot_wait.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
+# boot_wait.sh is back from the grave. undead undead undead
 
-max_counter=600
-sleep='sleep 1'
-
-counter=0
-success=0
+sleep='sleep 10'
 
 for process in "$@"
 do
@@ -12,26 +9,22 @@ do
    chmod 600 /tmp/boot_wait.txt
 
    # Run Process in BG
-   echo $process
-   nohup $process > /tmp/boot_wait.txt 2>&1 &
+   echo bin/boot_pipe_menu.exp $process
+   nohup bin/boot_pipe_menu.exp $process > /tmp/boot_wait.txt 2>&1 &
 
    success=0
-   while [ $counter -lt $max_counter -a $success -eq 0 ]; do
-      if [ `grep "\# " /tmp/boot_wait.txt | wc -l` -ge 1 ]; then
+   while [ $success -eq 0 ]; do
+      if [ `grep "Final Status: Booted" /tmp/boot_wait.txt | wc -l` -ge 1 ]; then
          success=1
       else
+         tail -n 1 /tmp/boot_wait.txt
+         echo
          `$sleep`
-         counter=`expr $counter + 1`
       fi
    done
 
    rm /tmp/boot_wait.txt
 done
 
-if [ $success -eq 1 ]; then
-   echo "Success"
-   exit 0
-fi
-
-echo "Fail"
-exit 1
+echo "Success"
+exit 0
