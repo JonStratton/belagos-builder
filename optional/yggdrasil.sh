e5eb6604999f8034efc0938257cb88013063516b
jgstratton <jgstratton@cyber.lan>
2024-06-20 08:33:18 -0500

    Updated for more modern debian, and 9front

--- optional/yggdrasil.sh
+++ optional/yggdrasil.sh
@@ -53,12 +53,6 @@ install()
 {
    # Only install it if not installed, in case its already installed and configured
    if [ ! -L /sys/class/net/tun0 ]; then
-      # Add get and repo for yggdrasil
-      gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
-      gpg --export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | sudo apt-key add -
-      gpg --batch --delete-keys 569130E8CA20FBC4CB3FDE555898470A764B32C9
-      sudo sh -c '( echo "deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil" > /etc/apt/sources.list.d/yggdrasil.list )'
-
       # Install Deps
       sudo apt-get update
       sudo DEBIAN_FRONTEND=noninteractive apt-get install -y yggdrasil
